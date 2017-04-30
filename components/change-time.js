8a85754534f9f91599e742b4521492d123684ce2
Marshall Levin <marshall_levin@epam.com>
2017-04-30 14:37:11 -0400

    water level

--- components/change-time.js
+++ components/change-time.js
@@ -27,21 +27,29 @@ AFRAME.registerComponent('change-time', {
                     document.querySelector('#point-light').setAttribute('light', 'intensity', 0.25);
                     document.querySelector('#point-light').setAttribute('position', { x: -10, y: 1, z: 0 });
                     data.target.setAttribute('material', 'src', '#sky-0000');
+                    document.querySelector('#water').setAttribute('geometry', 'height', .135);
+                    document.querySelector('#water').setAttribute('position', { x: 0, y: -0.175, z: 0 });
                     break;
                 case '6:15':
                     document.querySelector('#point-light').setAttribute('light', 'intensity', 1);
                     document.querySelector('#point-light').setAttribute('position', { x: -4, y: 4, z: 0 });
                     data.target.setAttribute('material', 'src', '#sky-0615');
+                    document.querySelector('#water').setAttribute('geometry', 'height', .1);
+                    document.querySelector('#water').setAttribute('position', { x: 0, y: -0.175, z: 0 });
                     break;
                 case '12:30':
                     document.querySelector('#point-light').setAttribute('light', 'intensity', 4);
                     document.querySelector('#point-light').setAttribute('position', { x: 3, y: 8, z: 0 });
                     data.target.setAttribute('material', 'src', '#sky-1230');
+                    document.querySelector('#water').setAttribute('geometry', 'height', .165);
+                    document.querySelector('#water').setAttribute('position', { x: 0, y: -0.145, z: 0 });
                     break;
                 case '18:45':
                     document.querySelector('#point-light').setAttribute('light', 'intensity', 0.25);
                     document.querySelector('#point-light').setAttribute('position', { x: 10, y: 1, z: 0 });
                     data.target.setAttribute('material', 'src', '#sky-1845');
+                    document.querySelector('#water').setAttribute('geometry', 'height', .2);
+                    document.querySelector('#water').setAttribute('position', { x: 0, y: -0.135, z: 0 });
                     break;
             }
         });
