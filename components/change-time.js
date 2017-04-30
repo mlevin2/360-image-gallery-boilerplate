640b44a32fc11f89c3a7fa36c8fd953c11cdad46
Marshall Levin <marshall_levin@epam.com>
2017-04-30 10:22:53 -0400

    Updates

--- components/change-time.js
+++ components/change-time.js
@@ -18,6 +18,24 @@ AFRAME.registerComponent('change-time', {
             document.querySelector('#time').setAttribute('text', 'value', data.time + '\nSOL 24');
             document.querySelector('#dashboard').setAttribute('material', 'src', data.dashboard);
             // TODO: Kris, change the lighting here
+            switch (data.time) {
+                case '0:15':
+                    document.querySelector('#point-light').setAttribute('light', 'intensity', 0.25);
+                    document.querySelector('#point-light').setAttribute('position', { x: -10, y: 1, z: 0 });
+                    break;
+                case '8:15':
+                    document.querySelector('#point-light').setAttribute('light', 'intensity', 1);
+                    document.querySelector('#point-light').setAttribute('position', { x: -4, y: 4, z: 0 });
+                    break;
+                case '16:15':
+                    document.querySelector('#point-light').setAttribute('light', 'intensity', 4);
+                    document.querySelector('#point-light').setAttribute('position', { x: 3, y: 8, z: 0 });
+                    break;
+                case '24:15':
+                    document.querySelector('#point-light').setAttribute('light', 'intensity', 0.25);
+                    document.querySelector('#point-light').setAttribute('position', { x: 10, y: 1, z: 0 });
+                    break;
+            }
         });
     }
 
